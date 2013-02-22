3c491f2c989ed998a1cd9470ca2a8e935a6584a7
dhampik <dhampik@gmail.com>
2013-02-22 17:05:52 +0400

    Debian installation description and post-install configuration

--- README.md
+++ README.md
@@ -1,4 +1,44 @@
 vagrant-boxer
 =============
 
-Some scripts and instructions which help to create custom vagrant box and configure it with chef
+This is what options are used by debian installer by default
+
+vagrant-box-debian-6.0.6-i386
+
+debian-squeeze-i386-6.0.6-netinst iso was used
+RAM size 384 MB
+VDI disk image
+Dynamic size 40 GB
+Settings->System->Motherboard->Boot order->Diskette was disabled
+Settings->Audio disabled
+Settings->Network = NAT
+
+Install
+Language: English
+Location: United States
+Keyboard layout: American English
+
+Hostname: vagrant
+Domain name: empty
+Root password: vagrant
+Main account login: vagrant
+Main account password: vagrant
+
+Timezone: Eastern
+
+Partitioning method: Guided - use entire disk
+All files on the same partition
+Swap partition size: 768 MB
+
+Debian archive mirror country: United States
+Debian archive mirror: ftp.us.debian.org
+
+No proxy
+
+No popularity-contest
+
+Additional packages: ssh server, standard system utilities
+
+GRUB installed to MBR
+
+To prepare virtual machine for packaging you should login as root and run post-install.sh
---
+++ post-install.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# THIS SCRIPT SHOULD BE EXECUTED MANUALLY BY root RIGHT AFTER DEBIAN INSTALLATION
+# This script prepares virtual box for packaging into vagrant box
+
+# break on first error
+set -e
+# show all executing commands
+set -x
+
+# Add admin group and add vagrant user to this group
+groupadd admin && usermod -a -G admin vagrant
+
+# install sudo
+apt-get -y --no-install-recommends install sudo
+
+# Retain SSH_AUTH_SOCK to use sudo with ssh agend
+# and avoid to remember sudo password for some time
+# by adding the following line to /etc/sudoers :
+# Defaults	env_keep	+= "SSH_AUTH_SOCK",timestamp_timeout=0
+sed -n 'H;${x;s/Defaults\senv_reset\n/&Defaults\tenv_keep\t+= "SSH_AUTH_SOCK",timestamp_timeout=0\n/;p;}' /etc/sudoers > /etc/sudoers.tmp && cp /etc/sudoers.tmp /etc/sudoers && rm /etc/sudoers.tmp
+
+# Disable password request for users in admin group
+# by adding the following line to /etc/sudoers :
+# %admin	ALL=NOPASSWD: ALL
+sed -n 'H;${x;s/root\sALL=(ALL)\sALL\n/&%admin\tALL=NOPASSWD: ALL\n/;p;}' /etc/sudoers > /etc/sudoers.tmp && cp /etc/sudoers.tmp /etc/sudoers && rm /etc/sudoers.tmp
+
+# Set up insecure public ssh key for vagrant user
+# (it'll be automatically used by vagrant)
+wget -P /home/vagrant https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
+test -d /home/vagrant/ssh || mkdir /home/vagrant/.ssh
+touch /home/vagrant/.ssh/authorized_keys
+cat /home/vagrant/vagrant.pub >> /home/vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant /home/vagrant/.ssh
+chmod 700 /home/vagrant/.ssh/
+chmod 600 /home/vagrant/.ssh/authorized_keys
+rm /home/vagrant/vagrant.pub
+
+# Disable DNS lookup and speed up connection
+# by adding the following line to /etc/ssh/sshd_config :
+# UseDNS no
+sed -n 'H;${x;s/TCPKeepAlive.*?\n/&UseDNS no/;p;}' /etc/ssh/sshd_config > /etc/ssh/sshd_config.tmp && cp /etc/ssh/sshd_config.tmp /etc/ssh/sshd_config && rm /etc/ssh/sshd_config.tmp
+
+# update all packages and clean up cache
+apt-get -y update && apt-get -y upgrade && apt-get -y autoremove && apt-get -y clean
+
+# blacklist pcspkr to avoid error msg on startup
+echo '# Disable pcspkr error msg on startup' >> /etc/modprobe.d/blacklist.conf
+echo 'blacklist pcspkr' >> /etc/modprobe.d/blacklist.conf
+
+# Clear bash history for root user
+history -d $((HISTCMD-1)) && \
+rm /root/.bash_history
