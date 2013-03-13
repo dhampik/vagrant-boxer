9855471112544605ceca4af493cd0c51eac66093
dhampik <dhampik@gmail.com>
2013-03-13 13:43:39 +0400

    Improved post-install shell script which performs basic configuration to make vagrant working fine

--- post-install.sh
+++ post-install.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-# THIS SCRIPT SHOULD BE EXECUTED MANUALLY BY root RIGHT AFTER DEBIAN INSTALLATION
+# THIS SCRIPT SHOULD BE EXECUTED MANUALLY BY root DIRECTLY AFTER DEBIAN INSTALLATION
 # This script prepares virtual box for packaging into vagrant box
 
 # break on first error
 set -e
 # show all executing commands
 set -x
+
+# update all packages
+apt-get -y update && apt-get -y upgrade
 
 # Add admin group and add vagrant user to this group
 groupadd admin && usermod -a -G admin vagrant
@@ -18,12 +21,17 @@ apt-get -y --no-install-recommends install sudo
 # and avoid to remember sudo password for some time
 # by adding the following line to /etc/sudoers :
 # Defaults	env_keep	+= "SSH_AUTH_SOCK",timestamp_timeout=0
-sed -n 'H;${x;s/Defaults\senv_reset\n/&Defaults\tenv_keep\t+= "SSH_AUTH_SOCK",timestamp_timeout=0\n/;p;}' /etc/sudoers > /etc/sudoers.tmp && cp /etc/sudoers.tmp /etc/sudoers && rm /etc/sudoers.tmp
+# !Note, that quantifiers should be escaped in sed, the contrary to PCRE
+cp /etc/sudoers /etc/sudoers.orig
+sed -i -e 's/Defaults\s\+env_reset/&\nDefaults\tenv_keep\t+= "SSH_AUTH_SOCK",timestamp_timeout=0/' /etc/sudoers
 
 # Disable password request for users in admin group
 # by adding the following line to /etc/sudoers :
 # %admin	ALL=NOPASSWD: ALL
-sed -n 'H;${x;s/root\sALL=(ALL)\sALL\n/&%admin\tALL=NOPASSWD: ALL\n/;p;}' /etc/sudoers > /etc/sudoers.tmp && cp /etc/sudoers.tmp /etc/sudoers && rm /etc/sudoers.tmp
+# !Note, that quantifiers should be escaped in sed, the contrary to PCRE
+sed -i -e 's/root\s\+ALL=(ALL)\s\+ALL/&\n%admin\tALL=NOPASSWD: ALL/' /etc/sudoers
+
+/etc/init.d/sudo restart
 
 # Set up insecure public ssh key for vagrant user
 # (it'll be automatically used by vagrant)
@@ -39,15 +47,45 @@ rm /home/vagrant/vagrant.pub
 # Disable DNS lookup and speed up connection
 # by adding the following line to /etc/ssh/sshd_config :
 # UseDNS no
-sed -n 'H;${x;s/TCPKeepAlive\s(yes|no)?\n/&UseDNS no\n/;p;}' /etc/ssh/sshd_config > /etc/ssh/sshd_config.tmp && cp /etc/ssh/sshd_config.tmp /etc/ssh/sshd_config && rm /etc/ssh/sshd_config.tmp
+# !Note, that quantifiers should be escaped in sed, the contrary to PCRE
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.orig
+sed -i -e 's/TCPKeepAlive\s\+yes/&\nUseDNS no/' /etc/ssh/sshd_config
 
-# update all packages and clean up cache
-apt-get -y update && apt-get -y upgrade && apt-get -y autoremove && apt-get -y clean
+/etc/init.d/ssh restart
+
+# Install latest virtualbox guest addititions
+cp /etc/apt/sources.list /ect/apt/sources.list.orig
+cat << EOF >> /etc/apt/sources.list
+
+# VirtualBox Guest Additions source for automatic installation from packages
+deb http://download.virtualbox.org/virtualbox/debian squeeze contrib non-free
+EOF
+
+wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | apt-key add -
+
+# update all package sources
+apt-get -y update
+
+# uninstall default virtual box guest additions
+apt-get -y purge virtualbox-ose-guest-dkms virtualbox-ose-guest-x11 virtualbox-ose-guest-utils
+
+# install latest virtualbox
+apt-get -y install virtualbox-4.2
+
+# remove not needed packages and clean aptitude cache
+apt-get -y autoremove && apt-get -y clean
 
 # blacklist pcspkr to avoid error msg on startup
-echo '# Disable pcspkr error msg on startup' >> /etc/modprobe.d/blacklist.conf
-echo 'blacklist pcspkr' >> /etc/modprobe.d/blacklist.conf
+cp /etc/modprobe.d/blacklist.conf /etc/modprobe.d/blacklist.conf.orig
+cat << EOF >> /etc/modprobe.d/blacklist.conf
+# Disable pcspkr error msg on startup
+blacklist pcspkr
+EOF
 
-# Clear bash history for root user
-history -d $((HISTCMD-1)) && \
-rm /root/.bash_history
+# Remove 5s grub timeout to speed up booting
+cp /etc/default/grub /etc/default/grub.orig
+sed -i -e 's/GRUB_TIMEOUT=\([0-9]\)\+/GRUB_TIMEOUT=0/' /etc/default/grub
+
+update-grub
+
+shutdown -r now
